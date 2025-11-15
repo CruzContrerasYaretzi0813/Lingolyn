80bee6b7b8493d7e289071cef562740519cc4f39
CruzContrerasYaretzi0813 <ycruzc2401@alumno.ipn.mx>
2025-11-15 16:22:25 -0600

    Primera Versión

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,57 @@
+package org.example;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Application
+{
+    static String nombre;
+
+    public static void main(String[] args)
+    {
+        moduloGeneral();
+    }
+
+    private static void moduloGeneral() {
+        Scanner sc = new Scanner(System.in);
+        char simbolo = (char) 42;
+        String palabra = "Lingolyn";
+        String titulo = String.valueOf(simbolo).repeat(8) + palabra + String.valueOf(simbolo).repeat(8);
+        System.out.println(titulo);
+        System.out.println("¡Bienvenido a Lingolyn!");
+        System.out.println("Por favor, ingresa tu nombre (como quieres que te llamemos);");
+        nombre = sc.nextLine();
+        System.out.println("Hola " + nombre + ", ¿Qué modo deseas practicar?");
+        System.out.println("1. Spelling Bee (deletreo)");
+        System.out.println("2. Vocabulario (ampliar palabras)");
+        System.out.println("Elige 1 o 2:");
+        int opc1 = sc.nextInt();
+        if (opc1 != 1 && opc1 != 2)
+        {
+            System.out.println("?");
+            System.out.println("Opción invalida");
+            System.out.println("Debes ingresar 1 o 2.");
+            System.out.println("Intenta de nuevo.");
+            opc1 = sc.nextInt();
+        } else if (opc1 == 1 || opc1 == 2)
+        {
+            if (opc1 == 1)
+            {
+                System.out.println(nombre + ", elegiste: Spelling Bee.");
+                System.out.println("Ahora elige la modalidad");
+                System.out.println("1. Modalidad Tradicional");
+                System.out.println("2. Modalidad Retos y Juegos");
+                System.out.println("Ingresa 1 o 2:");
+                int opcs = sc.nextInt();
+            }
+            else if (opc1 == 2)
+            {
+                System.out.println(nombre + ", elegiste: Vocabulario.");
+                System.out.println("Ahora elige la modalidad");
+                System.out.println("1. Modalidad Tradicional");
+                System.out.println("2. Modalidad Juegos");
+                System.out.println("Ingresa 1 o 2:");
+                int opcv = sc.nextInt();
+            }
+
+        }
+    }
+}
