66a10304f4eaebd0eb6b02a6b62fb2f2d90eebeb
Cruz Yaretzi <ycruzc2401@alumno.ipn.mx>
2025-11-15 17:08:48 -0600

    Update Application.java

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,15 +1,18 @@
 package org.example;
 import java.util.Scanner;
-public class Application
+public class Application 
 {
     static String nombre;
 
-    public static void main(String[] args)
+    public static void main(String[] args) 
     {
         moduloGeneral();
+        moduloTradicionalSpellingBe();
     }
+    
 
-    private static void moduloGeneral() {
+    private static void moduloGeneral() 
+    {
         Scanner sc = new Scanner(System.in);
         char simbolo = (char) 42;
         String palabra = "Lingolyn";
@@ -21,36 +24,41 @@ public class Application
         System.out.println("Hola " + nombre + ", ¿Qué modo deseas practicar?");
         System.out.println("1. Spelling Bee (deletreo)");
         System.out.println("2. Vocabulario (ampliar palabras)");
-        System.out.println("Elige 1 o 2:");
-        int opc1 = sc.nextInt();
-        if (opc1 != 1 && opc1 != 2)
-        {
-            System.out.println("?");
-            System.out.println("Opción invalida");
-            System.out.println("Debes ingresar 1 o 2.");
-            System.out.println("Intenta de nuevo.");
+        int opc1 = 0;
+        while (opc1 != 1 && opc1 != 2) {
+            System.out.println("Elige 1 o 2:");
             opc1 = sc.nextInt();
-        } else if (opc1 == 1 || opc1 == 2)
-        {
-            if (opc1 == 1)
-            {
-                System.out.println(nombre + ", elegiste: Spelling Bee.");
-                System.out.println("Ahora elige la modalidad");
-                System.out.println("1. Modalidad Tradicional");
-                System.out.println("2. Modalidad Retos y Juegos");
-                System.out.println("Ingresa 1 o 2:");
-                int opcs = sc.nextInt();
+            if (opc1 != 1 && opc1 != 2) {
+                System.out.println("?");
+                System.out.println("Opción invalida");
+                System.out.println("Debes ingresar 1 o 2.");
+                System.out.println("Intenta de nuevo.");
+                opc1 = sc.nextInt();
             }
-            else if (opc1 == 2)
-            {
-                System.out.println(nombre + ", elegiste: Vocabulario.");
-                System.out.println("Ahora elige la modalidad");
-                System.out.println("1. Modalidad Tradicional");
-                System.out.println("2. Modalidad Juegos");
-                System.out.println("Ingresa 1 o 2:");
-                int opcv = sc.nextInt();
-            }
+            if (opc1 == 1 || opc1 == 2) {
+                if (opc1 == 1) {
+                    System.out.println(nombre + ", elegiste: Spelling Bee.");
+                    System.out.println("Ahora elige la modalidad");
+                    System.out.println("1. Modalidad Tradicional");
+                    System.out.println("2. Modalidad Retos y Juegos");
+                    System.out.println("Ingresa 1 o 2:");
+                    int opcs = sc.nextInt();
+                    
 
+                } else if (opc1 == 2) {
+                    System.out.println(nombre + ", elegiste: Vocabulario.");
+                    System.out.println("Ahora elige la modalidad");
+                    System.out.println("1. Modalidad Tradicional");
+                    System.out.println("2. Modalidad Juegos");
+                    System.out.println("Ingresa 1 o 2:");
+                    int opcv = sc.nextInt();
+                }
+            }
         }
+    }
+    
+    private static void moduloTradicionalSpellingBe() 
+    {
+        
     }
 }
