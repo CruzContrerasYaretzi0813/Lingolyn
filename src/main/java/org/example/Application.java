32a8eb3ef1088e5ae92db525d21ed47186bc3172
CruzContrerasYaretzi0813 <ycruzc2401@alumno.ipn.mx>
2025-11-15 20:40:35 -0600

    Cambios en la parte if anidados anteriormente y los modulos

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,136 +1,134 @@
 package org.example;
-import java.util.Scanner;
-public class Application
+import java.util.Scanner; public class Application {
+        static String nombre;
+
+public static void main(String[] args)
 {
-    static String nombre;
+    moduloGeneral();
+    modalidadTradicionalSpellingBee();
+    modalidadRetosyJuegosSpellingBee();
+    modalidadTradicionalVocabulario();
+    modalidadJuegosVocabulario();
+}
 
-    public static void main(String[] args)
+
+
+private static void moduloGeneral()
+{
+    Scanner sc = new Scanner(System.in);
+    char simbolo = (char) 42;
+    String palabra = "Lingolyn";
+    String titulo = String.valueOf(simbolo).repeat(10) + palabra + String.valueOf(simbolo).repeat(10);
+    System.out.println(titulo);
+    System.out.println("¡Bienvenido a Lingolyn!");
+    System.out.println("Por favor, ingresa tu nombre (como quieres que te llamemos);");
+    nombre = sc.nextLine();
+    System.out.println("Hola " + nombre + ", ¿Qué modo deseas practicar?");
+    System.out.println("1. Spelling Bee (deletreo)");
+    System.out.println("2. Vocabulario (ampliar palabras)");
+    int opcionprincipal = 0;
+    while (opcionprincipal != 1 && opcionprincipal != 2)
     {
-        moduloGeneral();
-        modalidadTradicionalSpellingBee();
-        modalidadRetosyJuegosSpellingBee();
-        modalidadTradicionalVocabulario();
-        modalidadJuegosVocabulario();
-    }
-
-
-
-    private static void moduloGeneral()
-    {
-        Scanner sc = new Scanner(System.in);
-        char simbolo = (char) 42;
-        String palabra = "Lingolyn";
-        String titulo = String.valueOf(simbolo).repeat(10) + palabra + String.valueOf(simbolo).repeat(10);
-        System.out.println(titulo);
-        System.out.println("¡Bienvenido a Lingolyn!");
-        System.out.println("Por favor, ingresa tu nombre (como quieres que te llamemos);");
-        nombre = sc.nextLine();
-        System.out.println("Hola " + nombre + ", ¿Qué modo deseas practicar?");
-        System.out.println("1. Spelling Bee (deletreo)");
-        System.out.println("2. Vocabulario (ampliar palabras)");
-        int opcionprincipal = 0;
-        while (opcionprincipal != 1 && opcionprincipal != 2)
+        System.out.println("Elige 1 o 2:");
+        opcionprincipal = sc.nextInt();
+        if (opcionprincipal != 1 && opcionprincipal != 2)
         {
-            System.out.println("Elige 1 o 2:");
+            System.out.println("?");
+            System.out.println("Opción invalida");
+            System.out.println("Debes ingresar 1 o 2.");
+            System.out.println("Intenta de nuevo.");
             opcionprincipal = sc.nextInt();
-            if (opcionprincipal != 1 && opcionprincipal != 2)
+        }
+        if (opcionprincipal == 1 || opcionprincipal == 2)
+        {
+            switch (opcionprincipal)
             {
-                System.out.println("?");
-                System.out.println("Opción invalida");
-                System.out.println("Debes ingresar 1 o 2.");
-                System.out.println("Intenta de nuevo.");
-                opcionprincipal = sc.nextInt();
-            }
-            if (opcionprincipal == 1 || opcionprincipal == 2)
-            {
-                switch (opcionprincipal)
-                {
-                    case 1:
-                        System.out.println(nombre + ", elegiste: Spelling Bee.");
-                        System.out.println("Ahora elige la modalidad");
-                        System.out.println("1. Modalidad Tradicional");
-                        System.out.println("2. Modalidad Retos y Juegos");
-                        int modalidadSpellingBee = 0;
-                        while ( modalidadSpellingBee != 1 && modalidadSpellingBee !=2 )
+                case 1:
+                    System.out.println(nombre + ", elegiste: Spelling Bee.");
+                    System.out.println("Ahora elige la modalidad");
+                    System.out.println("1. Modalidad Tradicional");
+                    System.out.println("2. Modalidad Retos y Juegos");
+                    int modalidadSpellingBee = 0;
+                    while ( modalidadSpellingBee != 1 && modalidadSpellingBee !=2 )
+                    {
+                        System.out.println("Ingresa 1 o 2:");
+                        modalidadSpellingBee = sc.nextInt();
+                        if (modalidadSpellingBee != 1 && modalidadSpellingBee != 2)
                         {
-                            System.out.println("Ingresa 1 o 2:");
+                            System.out.println("?");
+                            System.out.println("Opción invalida");
+                            System.out.println("Esa opción no corresponde a ninguna modalidad.");
+                            System.out.println("Intenta nuevamente.");
                             modalidadSpellingBee = sc.nextInt();
-                            if (modalidadSpellingBee != 1 && modalidadSpellingBee != 2)
-                            {
-                                System.out.println("?");
-                                System.out.println("Opción invalida");
-                                System.out.println("Esa opción no corresponde a ninguna modalidad.");
-                                System.out.println("Intenta nuevamente.");
-                                modalidadSpellingBee = sc.nextInt();
-                            }
-                            if ( modalidadSpellingBee == 1 || modalidadSpellingBee ==2 )
-                            {
-                                switch (modalidadSpellingBee)
-                                {
-                                    case 1:
-                                        modalidadTradicionalSpellingBee();
-                                        break;
-                                    case 2:
-                                        modalidadRetosyJuegosSpellingBee();
-                                        break;
-                                }
-                            }
                         }
-                        break;
-
-                    case 2:
-                        System.out.println(nombre + ", elegiste: Vocabulario.");
-                        System.out.println("Ahora elige la modalidad");
-                        System.out.println("1. Modalidad Tradicional");
-                        System.out.println("2. Modalidad Juegos");
-                        int modalidadVocabulario = 0;
-                        while ( modalidadVocabulario != 1 && modalidadVocabulario !=2 )
+                        if ( modalidadSpellingBee == 1 || modalidadSpellingBee ==2 )
                         {
-                            System.out.println("Ingresa 1 o 2:");
-                            modalidadVocabulario = sc.nextInt();
-                            if (modalidadVocabulario != 1 && modalidadVocabulario != 2)
+                            switch (modalidadSpellingBee)
                             {
-                                System.out.println("?");
-                                System.out.println("Opción invalida");
-                                System.out.println("Esa opción no corresponde a ninguna modalidad.");
-                                System.out.println("Intenta nuevamente.");
-                                modalidadVocabulario = sc.nextInt();
-                            }
-                            if ( modalidadVocabulario == 1 || modalidadVocabulario ==2 )
-                            {
-                                switch (modalidadVocabulario)
-                                {
-                                    case 1:
-
-                                        break;
-                                    case 2:
-
-                                        break;
-                                }
+                                case 1:
+                                    modalidadTradicionalSpellingBee();
+                                    break;
+                                case 2:
+                                    modalidadRetosyJuegosSpellingBee();
+                                    break;
                             }
                         }
+                    }
+                    break;
 
-                        break;
-                }
+                case 2:
+                    System.out.println(nombre + ", elegiste: Vocabulario.");
+                    System.out.println("Ahora elige la modalidad");
+                    System.out.println("1. Modalidad Tradicional");
+                    System.out.println("2. Modalidad Juegos");
+                    int modalidadVocabulario = 0;
+                    while ( modalidadVocabulario != 1 && modalidadVocabulario !=2 )
+                    {
+                        System.out.println("Ingresa 1 o 2:");
+                        modalidadVocabulario = sc.nextInt();
+                        if (modalidadVocabulario != 1 && modalidadVocabulario != 2)
+                        {
+                            System.out.println("?");
+                            System.out.println("Opción invalida");
+                            System.out.println("Esa opción no corresponde a ninguna modalidad.");
+                            System.out.println("Intenta nuevamente.");
+                            modalidadVocabulario = sc.nextInt();
+                        }
+                        if ( modalidadVocabulario == 1 || modalidadVocabulario ==2 )
+                        {
+                            switch (modalidadVocabulario)
+                            {
+                                case 1:
+                                    modalidadTradicionalVocabulario();
+                                    break;
+                                case 2:
+                                    modalidadJuegosVocabulario();
+                                    break;
+                            }
+                        }
+                    }
+
+                    break;
             }
         }
     }
+}
 
-    private static void modalidadTradicionalSpellingBee()
-    {
+private static void modalidadTradicionalSpellingBee()
+{
 
-    }
+    System.out.println("Has elegido la Modalidad Tradicional");
+}
 
-    private static void modalidadRetosyJuegosSpellingBee()
-    {
+private static void modalidadRetosyJuegosSpellingBee()
+{
+    System.out.println("Has elegido la modalidad Retos y Juegos");
+}
+private static void modalidadTradicionalVocabulario()
+{
 
-    }
-    private static void modalidadTradicionalVocabulario()
-    {
+}
+private static void modalidadJuegosVocabulario()
+{
 
-    }
-    private static void modalidadJuegosVocabulario()
-    {
-
-    }
 }
